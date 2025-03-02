a9cfeb9449485482ce7f36996a8a6d54410e119d
Zikuang Huang <119480641+jonashuang0904@users.noreply.github.com>
2025-03-02 15:17:54 +0100

    Create code_is_private.cpp

---
+++ code_is_private.cpp
@@ -0,0 +1,6 @@
+// The code I wrote in this project is private. 
+// It is actively used by the KA-RaceIng team.
+// It is the core of velocity planning at KA-RaceIng and essential for our success in autonomous racing. 
+// Hence, I cannot publish any of this code here. 
+
+// I can give you more insights in a personal discussion, though.
